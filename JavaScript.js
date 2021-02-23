a7aa3d00b7bba5221605b06605d74e72ca3121c6
Cassie Chamberlain <CHAMBERLAINCASSIE@YAHOO.COM>
2021-02-22 21:13:30 -0500

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,9 @@
+var Family = "The Peets", Dad = " Josh is the best Dad ever!", Mom = "Cassie", Sister = "Arya"
+    Brother = "Aiden";
+var Family = Family.fontcolor("green");
+var Dad = Dad.fontcolor("blue");
+var Mom = Mom.fontcolor("purple");
+var Sister = Sister.fontcolor("pink");
+var Brother = Brother.fontcolor("red");
+document.write(Family + Dad);
+      
