8295e003b341d82afaa35aaeb72deee7da2f951e
Cassie Chamberlain <CHAMBERLAINCASSIE@YAHOO.COM>
2021-02-22 21:34:34 -0500

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,9 @@
 var Family = "The Peets", Dad = " Josh is the best Dad ever!", Mom = "Cassie", Sister = "Arya"
-    Brother = "Aiden";
-var Family = Family.fontcolor("green");
-var Dad = Dad.fontcolor("blue");
-var Mom = Mom.fontcolor("purple");
-var Sister = Sister.fontcolor("pink");
-var Brother = Brother.fontcolor("red");
-document.write(Family + Dad);
+    Brother = "Aiden";// naming and defineing my Variables
+var Family = Family.fontcolor("green");//font color
+var Dad = Dad.fontcolor("blue");//font color
+var Mom = Mom.fontcolor("purple");//font color
+var Sister = Sister.fontcolor("pink");//font color
+var Brother = Brother.fontcolor("red");//font color
+document.write(Family + Dad);//concatenate
       
